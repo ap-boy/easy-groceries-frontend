0d468e706fae295b1d8f90c42e03d709db7f5789
Andrew Boy <andrew.boy@entelect.co.uk>
2022-11-16 18:33:53 +0000

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { Navbar } from './components/Navbar';
+import { Home } from './components/Home';
+import { Store } from './components/Store';
+import { NotFound } from './components/NotFound';
+import { About } from './components/About';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Container>
     </div>
   );
 }
