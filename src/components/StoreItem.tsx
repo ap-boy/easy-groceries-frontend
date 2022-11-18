b2e6ea1e5348c527dac5f620cab20d0cb050670d
Andrew Boy <andrew.boy@entelect.co.uk>
2022-11-18 20:17:53 +0000

    rename item to store item and add description

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,20 +1,21 @@
 
 import { Button, Card } from 'react-bootstrap';
 
-type ItemProps = {
-  id: number;
-  name: string;
-  price: number;
+type StoreItemProps = {
+  id: number,
+  name: string,
+  price: number,  
+  description: string
 };
 
-export function Item({ id, name, price }: ItemProps) {
-
+export function StoreItem({ id, name, price, description }: StoreItemProps) {
   return (
     <>
     <Card style={{ width: '18rem' }}>
         <Card.Body>
             <Card.Title>{name}</Card.Title>
             <Card.Text>£:{price}</Card.Text>
+            <Card.Text>{description}</Card.Text>
             <Button variant="primary">Add to Cart</Button>
         </Card.Body>
     </Card>
